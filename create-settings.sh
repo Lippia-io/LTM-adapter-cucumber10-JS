31ef3d0bbca145bc85e4e5963c47c2ddde944280
Juan Pablo Peña <juanpablo.pena@crowdaronline.com>
2024-02-28 19:27:27 +0000

    added sh changes

--- create-settings.sh
+++ create-settings.sh
@@ -3,13 +3,13 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 GITIGNORE=$DIR/.gitignore
 
-if [ -z "$NEXUS_REGISTRY_URL" ] || [ -z "$NEXUS_USERNAME" ] || [ -z "$NEXUS_PASSWORD" ]; then
-    >&2 echo "Error - NEXUS_REGISTRY_URL, NEXUS_USERNAME, and NEXUS_PASSWORD environment variables are required"
+if [ -z "$NEXUS_REGISTRY_URL" ] || [ -z "$NEXUS_USERNAME" ] || [ -z "$NEXUS_PASSPHRASE" ]; then
+    >&2 echo "Error - NEXUS_REGISTRY_URL, NEXUS_USERNAME, and NEXUS_PASSPHRASE environment variables are required"
     exit 1
 fi
 
 echo "registry=$NEXUS_REGISTRY_URL" > $DIR/.npmrc
-echo "_auth=$(echo -n "$NEXUS_USERNAME:$NEXUS_PASSWORD" | base64)" >> $DIR/.npmrc
+echo "_auth=$(echo -n "$NEXUS_USERNAME:$NEXUS_PASSPHRASE" | base64)" >> $DIR/.npmrc
 
 if grep -q "^\.npmrc$" $GITIGNORE; then
     echo "Success - .npmrc found in $GITIGNORE file. This will prevent git from saving the Nexus registry URL and credentials in version control."
